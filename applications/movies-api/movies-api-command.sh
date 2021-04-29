68affe5aa796444eaa002805b72e5447973381b2
ophir.h <ophir.h@quali.com>
2021-04-29 23:28:06 +0300

    plant var cra way

--- applications/movies-api/movies-api-command.sh
+++ applications/movies-api/movies-api-command.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 echo '=============== Staring start script for Promotions Manager API ==============='
+cd app
+npm start &
--- applications/movies-ui/movies-ui-command.sh
+++ applications/movies-ui/movies-ui-command.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 echo '=============== Staring start script for MOVIES UI ==============='
+export REACT_APP_COLONY_DOMAIN_NAME=$COLONY_DOMAIN_NAME
+cd app
+npm start &
