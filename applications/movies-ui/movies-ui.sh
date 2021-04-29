e752de0f9f1d737c32cc7fd98a204baed78eea81
ophir.h <ophir.h@quali.com>
2021-04-29 12:30:33 +0300

    start app

--- applications/movies-ui/movies-ui.sh
+++ applications/movies-ui/movies-ui.sh
@@ -4,8 +4,10 @@ echo $COLONY_DOMAIN_NAME
 
 echo '==> Installing Node.js and NPM'
 apt update -y
-apt install -y unzip nodejs
+apt install -y unzip nodejs npm
 
 echo '==> Extract ui artifact to app'
 unzip $ARTIFACTS_PATH/*.zip -d app
-ls app
+cd app
+npm install
+npm start
