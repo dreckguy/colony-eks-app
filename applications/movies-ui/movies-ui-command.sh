5521edba0bc40b88369da794d77d23d10b7eb806
ophir.h <ophir.h@quali.com>
2021-04-30 11:41:27 +0300

    use export

--- applications/movies-ui/movies-ui-command.sh
+++ applications/movies-ui/movies-ui-command.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo '=============== Staring start script for MOVIES UI ==============='
 cd app
-PORT=8000 REACT_APP_COLONY_DOMAIN_NAME=bla
+export PORT=8000 export REACT_APP_COLONY_DOMAIN_NAME=bla
 npm start &
