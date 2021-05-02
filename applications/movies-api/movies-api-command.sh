210ceb549223a669a8465d56778dd2c841c08a59
ophir.h <ophir.h@quali.com>
2021-05-02 15:03:29 +0300

    export COLONY_DOMAIN_NAME

--- applications/movies-api/movies-api-command.sh
+++ applications/movies-api/movies-api-command.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo '=============== Staring start script for MOVIES API ==============='
 cd app
-export MOVIES_API=$COLONY_PUBLIC_ADDRESS:7000
+export DOMAIN_NAME=$COLONY_DOMAIN_NAME
 npm start &
