ef72d422b73e5ac798c1f53cc136b6b358d889a2
ophir.h <ophir.h@quali.com>
2021-05-02 12:37:28 +0300

    change zero movies to success result

--- server/controllers/movie-ctrl.js
+++ server/controllers/movie-ctrl.js
@@ -102,11 +102,7 @@ getMovies = async (req, res) => {
         if (err) {
             return res.status(400).json({ success: false, error: err })
         }
-        if (!movies.length) {
-            return res
-                .status(404)
-                .json({ success: false, error: `Movie not found` })
-        }
+        
         return res.status(200).json({ success: true, data: movies })
     }).catch(err => console.log(err))
 }
