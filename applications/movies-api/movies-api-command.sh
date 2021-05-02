d01fb260d91f98d44d340122a10828e155f57594
ophir.h <ophir.h@quali.com>
2021-05-02 15:05:39 +0300

    fix indent

--- applications/movies-api/movies-api-command.sh
+++ applications/movies-api/movies-api-command.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo '=============== Staring start script for MOVIES API ==============='
 cd app
-export DOMAIN_NAME=$COLONY_DOMAIN_NAME
+export COLONY_DOMAIN_NAME=$COLONY_DOMAIN_NAME
 npm start &
