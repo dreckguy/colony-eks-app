2798b897fd1642b0e69177679c676c88b59c4aaf
ophir.h <ophir.h@quali.com>
2021-05-27 20:08:12 +0300

    add timeout to db

--- movies-api/db/index.js
+++ movies-api/db/index.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-    .connect(`mongodb://db:27017/cinema`, { useNewUrlParser: true })
+    .connect(`mongodb://db:27017/cinema`, { useNewUrlParser: true,server: {socketOptions: {socketTimeoutMS: 10000}}})
     .catch(e => {
         console.error('Connection error', e.message)
     })
