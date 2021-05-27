ce65c503168454ea06ecf0970515b1f9b44b8052
ophir.h <ophir.h@quali.com>
2021-05-27 16:00:25 +0300

    split compose for artifacts

--- applications/movies-app/movies-app.sh
+++ applications/movies-app/movies-app.sh
@@ -8,12 +8,12 @@ systemctl enable --now docker
 docker --version
 
 curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 docker–compose –version
 
-echo '==> Extract ui artifact to app'
+echo '==> Extract artifact to app'
 mkdir -p app
-cp $ARTIFACTS_PATH/docker-compose.yml -d app
+cp $ARTIFACTS_PATH/docker-compose.yml app
 chmod -R 777 app
 cd app
 docker-compose pull
