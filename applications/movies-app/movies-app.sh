5b7a99fa7442d8472e713823c53ed23ffc795b24
ophir.h <ophir.h@quali.com>
2021-05-27 12:24:44 +0300

    install docker

--- applications/movies-app/movies-app.sh
+++ applications/movies-app/movies-app.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 echo '=============== Staring init script for MOVIES APP ==============='
 
-echo '==> Installing docker and docker-compose'
-echo "need to install docker and compose!"
+apt update -y
+apt upgrade -y
+apt install -y docker.io
+systemctl enable --now docker
+docker --version
+docker–compose –version
+
 echo '==> Extract ui artifact to app'
-unzip $ARTIFACTS_PATH/docker-compose.yml -d app
+mkdir -p app
+cp $ARTIFACTS_PATH/docker-compose.yml -d app
 chmod -R 777 app
 cd app
 docker-compose pull
