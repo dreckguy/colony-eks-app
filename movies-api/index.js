1942c51094be39e9ae67f79f829b847c597cc93b
ophir.h <ophir.h@quali.com>
2021-05-27 16:54:34 +0300

    fix api port

--- movies-api/index.js
+++ movies-api/index.js
@@ -8,7 +8,7 @@ const db = require('./db')
 const movieRouter = require('./routes/movie-router')
 
 const app = express()
-const apiPort = 4000
+const apiPort = 7000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
