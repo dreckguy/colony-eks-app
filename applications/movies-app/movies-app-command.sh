cd0a81f4d75b257fc1c8cb9abfeb6f11529cc696
ophir.h <ophir.h@quali.com>
2021-05-27 16:07:46 +0300

    add external connectivity

--- applications/movies-app/movies-app-command.sh
+++ applications/movies-app/movies-app-command.sh
@@ -2,4 +2,4 @@
 echo '=============== Staring start script for MOVIES UI ==============='
 cd app
 #export PORT=8000 export REACT_APP_COLONY_DOMAIN_NAME=$COLONY_PUBLIC_ADDRESS
-#docker-compose up -d
+docker-compose up -d
