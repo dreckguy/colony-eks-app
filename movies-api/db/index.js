f8023ff4af52ce3c17decdd70dcbf045e05e5774
ophir.h <ophir.h@quali.com>
2021-06-03 17:27:23 +0300

    add all needed applications

--- movies-api/db/index.js
+++ movies-api/db/index.js
@@ -1,9 +1,9 @@
-//const {COLONY_DOMAIN_NAME} = process.env
+const {DB_URL} = process.env
 
 const mongoose = require('mongoose')
 
 mongoose
-    .connect(`mongodb://db:27017/cinema`, { useNewUrlParser: true,server: {socketOptions: {socketTimeoutMS: 10000}}})
+    .connect(`mongodb://${DB_URL}:27017/cinema`, { useNewUrlParser: true,server: {socketOptions: {socketTimeoutMS: 10000}}})
     .catch(e => {
         console.error('Connection error', e.message)
     })
