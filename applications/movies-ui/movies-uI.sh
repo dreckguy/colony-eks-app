8b63201c7a18c006da15b45dea7d7cb7d22b2c5c
ophir.h <ophir.h@quali.com>
2021-06-17 17:49:09 +0300

    echo ip

--- applications/movies-ui/movies-uI.sh
+++ applications/movies-ui/movies-uI.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "INIT SCRIPT !!!!!!!!!!!!!!!!!!"
-export API_URL=$(printenv | grep -i 'api'| grep -i 'host' | cut -d "=" -f2)
-echo "API_URL: $API_URL"
+printenv | grep -i 'api'| grep -i 'host' | cut -d "=" -f2
+#echo "API_URL: $API_URL"
